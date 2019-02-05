20f66645aa2a70e84f6c866111036820ccf6646f
MorentDennis <dennis.morent@student.howest.be>
2019-02-05 11:25:56 +0100

    refactoring into filter interface + read/update according to query/customid

---
+++ src/app/shared/models/Filter.ts
@@ -0,0 +1,5 @@
+export interface Filter {
+    fieldPath: string;
+    fieldValue: string;
+    opStr: any; 
+}
--- src/app/shared/services/firestore/firestore.service.ts
+++ src/app/shared/services/firestore/firestore.service.ts
@@ -1,5 +1,6 @@
+import { Filter } from "./../../models/Filter";
 import { Injectable } from "@angular/core";
-import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFirestore, DocumentSnapshot } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
 
 @Injectable({
@@ -27,26 +28,70 @@ export class FirestoreService {
     collection: string,
     start: Date,
     end: Date,
-    fieldPath: string,
-    fieldValue: string,
-    opStr: any
+    filter: Filter
   ): Observable<any[]> {
     return this._firestore
       .collection(collection, ref =>
         ref
           .where("created", ">", start)
           .where("created", "<", end)
-          .where(fieldPath, opStr, fieldValue)
+          .where(filter.fieldPath, filter.opStr, filter.fieldValue)
           .orderBy("created")
       )
       .valueChanges();
   }
 
-  public async addToCollection(collection: string, object: any): Promise<void> {
+  public async addToCollection(
+    collection: string,
+    object: any[] | any,
+    customId?: string[] | any
+  ): Promise<void> {
     try {
-      await this._firestore.collection(collection).add(object);
+      if (object.length) {
+        this.addObjectsToCollection(collection, object, customId);
+      } else {
+        this.addObjectToCollection(collection, object, customId);
+      }
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  private async addObjectToCollection(
+    collection: string,
+    object: any,
+    customId?: string
+  ) {
+    try {
+      if (customId) {
+        await this._firestore
+          .collection(collection)
+          .doc(customId)
+          .set(object);
+      } else {
+        await this._firestore.collection(collection).add(object);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private addObjectsToCollection(
+    collection: string,
+    objects: any[],
+    customIds?: string[]
+  ) {
+    if (customIds) {
+      objects.forEach(async (o, i) => {
+        await this._firestore
+          .collection(collection)
+          .doc(customIds[i])
+          .set(o);
+      });
+    } else {
+      objects.forEach(async o => {
+        await this._firestore.collection(collection).add(o);
+      });
     }
   }
 
@@ -54,37 +99,108 @@ export class FirestoreService {
     return this._firestore.collection(collection).valueChanges();
   }
 
+  // asumes unique names
   public async getDocumentFieldValue(
     collection: string,
-    fieldPath: string,
-    opStr: any,
-    fieldValue: string,
-    property: string
+    field: string,
+    customId?: string,
+    filter?: Filter
   ) {
-    const querySnapshot = await this._firestore
-      .collection(collection, ref => ref.where(fieldPath, opStr, fieldValue))
-      .get()
-      .toPromise();
-    const docData = querySnapshot.docs[0].data();
-    console.log(docData);
-    return docData[property];
+    if (filter) {
+      return this.getFieldValueWithQuery(collection, field, filter);
+    }
+    return this.getFieldValueWithCustomId(collection, field, customId);
   }
 
-  public async updateDocumentWithFilter(
+  private async getFieldValueWithQuery(
     collection: string,
-    fieldPath: string,
-    opStr: any,
-    fieldValue: string,
-    updatedField: string,
-    newValue: any
+    field: string,
+    filter: Filter
   ) {
-    const querySnapshot = await this._firestore
-      .collection(collection, ref => ref.where(fieldPath, opStr, fieldValue))
-      .get()
-      .toPromise();
-    const docRef = querySnapshot.docs[0].ref; // accountNames, categoryNames are unique
-    const obj = {};
-    obj[updatedField] = newValue;
-    await docRef.update(obj);
+    try {
+      const querySnapshot = await this._firestore
+        .collection(collection, ref =>
+          ref.where(filter.fieldPath, filter.opStr, filter.fieldValue)
+        )
+        .get()
+        .toPromise();
+      const docData = querySnapshot.docs[0].data();
+      return docData[field];
+    } catch (error) {}
+  }
+  private async getFieldValueWithCustomId(
+    collection: string,
+    field: string,
+    customId: string
+  ) {
+    try {
+      const object = await this._firestore
+        .collection(collection)
+        .doc(customId)
+        .get()
+        .toPromise();
+      return object.data()[field];
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async updateDocumentFieldValue(
+    collection: string,
+    field: string,
+    newValue: any,
+    customId?: string,
+    filter?: Filter,
+  ) {
+    if (filter) {
+      return await this.updateDocumentWithQuery(
+        collection,
+        field,
+        newValue,
+        filter
+      );
+    }
+    return await this.updateDocumentWithCustomId(
+      collection,
+      field,
+      newValue,
+      customId
+    );
+  }
+
+  private async updateDocumentWithQuery(
+    collection: string,
+    field: string,
+    newValue: any,
+    filter: Filter
+  ) {
+    try {
+      const querySnapshot = await this._firestore
+        .collection(collection, ref =>
+          ref.where(filter.fieldPath, filter.opStr, filter.fieldValue)
+        )
+        .get()
+        .toPromise();
+      const docRef = querySnapshot.docs[0].ref;
+      const obj = {};
+      obj[field] = newValue;
+      await docRef.update(obj);
+    } catch (error) {}
+  }
+
+  private async updateDocumentWithCustomId(
+    collection: string,
+    field: string,
+    newValue: any,
+    customId: string
+  ) {
+    try {
+      const obj = {};
+      obj[field] = newValue;
+      await this._firestore
+        .collection(collection)
+        .doc(customId)
+        .update(obj);
+    } catch (error) {}
   }
 }
